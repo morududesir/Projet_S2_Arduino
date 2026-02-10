e0b8194500818b3f8e5710ac060b347732ff95ba
JaydenBeauchesne <beaj5098@usherbrooke.ca>
2026-02-10 12:13:50 -0500

    Acqu

--- src/acquisition.h
+++ src/acquisition.h
@@ -3,10 +3,21 @@
 #ifndef ACQUISITION_H
 #define ACQUISTION_H
 
+#include <Arduino.h>
+
 const int PIN_POTENTIO1 = 0;
 const int PIN_POTENTIO2 = 0;
 const int PIN_JOYSTICKX = 0;
 const int PIN_JOYSTICKY = 0;
 const int PIN_ACCELERO = 0;
+
+void setup()
+{
+    pinMode(PIN_POTENTIO1,INPUT);
+    pinMode(PIN_POTENTIO2,INPUT);
+    pinMode(PIN_JOYSTICKX,INPUT);
+    pinMode(PIN_JOYSTICKY,INPUT);
+    pinMode(PIN_ACCELERO,INPUT);
+}
 
 #endif 
