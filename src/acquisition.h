18a6ff766afffc4ccd6dc45c54268e06e4e02178
JaydenBeauchesne <beaj5098@usherbrooke.ca>
2026-02-10 12:07:31 -0500

    Fichier entête

---
+++ src/acquisition.h
@@ -0,0 +1,12 @@
+//Fichier d'entête pour les fonctions d'acquisition de données pour les capteurs du volant
+
+#ifndef ACQUISITION_H
+#define ACQUISTION_H
+
+const int PIN_POTENTIO1 = 0;
+const int PIN_POTENTIO2 = 0;
+const int PIN_JOYSTICKX = 0;
+const int PIN_JOYSTICKY = 0;
+const int PIN_ACCELERO = 0;
+
+#endif 
