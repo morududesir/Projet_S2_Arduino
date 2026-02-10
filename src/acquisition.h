4b516839208e7641162cccae005049c4334d017a
JaydenBeauchesne <beaj5098@usherbrooke.ca>
2026-02-10 12:19:37 -0500

    Ajustement

--- src/acquisition.h
+++ src/acquisition.h
@@ -5,11 +5,13 @@
 
 #include <Arduino.h>
 
-const int PIN_POTENTIO1 = 0;
-const int PIN_POTENTIO2 = 0;
+const int PIN_POTENTIO1 = 21;
+const int PIN_POTENTIO2 = 20;
 const int PIN_JOYSTICKX = 0;
 const int PIN_JOYSTICKY = 0;
-const int PIN_ACCELERO = 0;
+const int PIN_ACCELEROX = A0;
+const int PIN_ACCELEROY = A1;
+const int PIN_ACCELEROZ = A2;
 
 void setup()
 {
@@ -17,7 +19,9 @@ void setup()
     pinMode(PIN_POTENTIO2,INPUT);
     pinMode(PIN_JOYSTICKX,INPUT);
     pinMode(PIN_JOYSTICKY,INPUT);
-    pinMode(PIN_ACCELERO,INPUT);
+    pinMode(PIN_ACCELEROX,INPUT);
+    pinMode(PIN_ACCELEROY,INPUT);
+    pinMode(PIN_ACCELEROZ,INPUT);
 }
 
 #endif 
