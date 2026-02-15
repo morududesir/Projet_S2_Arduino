5a8fbd10e3c919280b65519ccbd33c964b68e583
JaydenBeauchesne <beaj5098@usherbrooke.ca>
2026-02-15 01:48:47 -0500

    Code pour traitement accel

--- src/traitement.cpp
+++ src/traitement.cpp
@@ -15,3 +15,32 @@ uint8_t traitementJoystick() {
     }
 
 }
+
+
+float traitement_acceleration()
+{
+    //Initialisation variables
+    float ratio_V_en_G = 0.3;
+    Acceleration acc_en_g = capterAccel();
+    float XYZ[3] = {acc_en_g.x,acc_en_g.y,acc_en_g.z}; //Extraction valeurs accel XYZ
+    float XYZ_en_g[3];
+    int i;
+    Direction direction;
+
+    //Conversion V en g
+    for(i=0;i<sizeof(XYZ);i++){
+        if(XYZ[i]<1.45 && XYZ[i]>1.65){
+            XYZ_en_g[i] = (XYZ[i]-V_0g)/ratio_V_en_G;
+        }
+        else{
+            XYZ[i] = 0.0;
+        }
+    }
+
+    //Détection direction accelération
+    if(XYZ[0]>0 && XYZ[1)
+
+
+
+
+}
--- src/traitement.h
+++ src/traitement.h
@@ -10,4 +10,8 @@ uint8_t traitementJoystick();
 const int joy_deadzone = 50;
 const int joy_mid = 512;
 
+const float V_0g = 1.5; //Min 0g
+
+const float ratio_V_en_g = (1.95/1)
+
 #endif
